a87aedc2a417988db263f6dcc1b64f0b0d19caf3
sudip55 <sudipta_roy1976@yahoo.com>
2016-03-02 23:48:56 +0530

    Added files via upload

--- src/main/java/uk/co/jemos/podam/common/AbstractMethodComparator.java
+++ src/main/java/uk/co/jemos/podam/common/AbstractMethodComparator.java
@@ -3,7 +3,7 @@
  */
 package uk.co.jemos.podam.common;
 
-import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Comparator;
 
@@ -11,32 +11,32 @@ import java.util.Comparator;
  * It provides a comparator to sort the constructor to choose first.
  * <p>
  * The priority goes to constructors with the {@link PodamConstructor}
- * annotation first, and then to those with less arguments.
+ * annotation first, and then to those with more arguments.
  * </p>
  * 
- * @author daivanov
+ * @author tedonema
  *
  */
-public abstract class AbstractConstructorComparator implements Comparator<Constructor<?>> {
+public abstract class AbstractMethodComparator implements Comparator<Method> {
 
-	public int constructorComplexity(Constructor<?> constructor) {
+	public int methodComplexity(Method method) {
 
 		int complexity = 0;
-		for (Class<?> parameter : constructor.getParameterTypes()) {
+		for (Class<?> parameter : method.getParameterTypes()) {
 			if (parameter.isInterface()
 					|| (Modifier.isAbstract(parameter.getModifiers()) && !parameter.isPrimitive())
-					|| parameter.isAssignableFrom(constructor.getDeclaringClass())) {
+					|| parameter.isAssignableFrom(method.getDeclaringClass())) {
 				complexity++;
 			}
 		}
 		return complexity;
 	}
 
-	public int compareAnnotations(Constructor<?> constructor1, Constructor<?> constructor2) {
+	public int compareAnnotations(Method method1, Method method2) {
 
 		/* Constructors with Podam annotation first */
-		boolean choose1 = constructor1.getAnnotation(PodamConstructor.class) != null;
-		boolean choose2 = constructor2.getAnnotation(PodamConstructor.class) != null;
+		boolean choose1 = method1.getAnnotation(PodamConstructor.class) != null;
+		boolean choose2 = method2.getAnnotation(PodamConstructor.class) != null;
 		if (choose1 && !choose2) {
 			return Integer.MIN_VALUE;
 		} else if (!choose1 && choose2) {
